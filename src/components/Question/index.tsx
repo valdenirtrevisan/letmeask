aca17f76cda4b926f45b6a72a034df2044dbf622
Valdenir Trevisan Junior <valdenirtrevi@gmail.com>
2021-06-26 10:38:15 -0300

    Aula 05

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react';
+import cx from 'classnames';
+
 import './styles.scss';
 
 type QuestionProps = {
@@ -8,20 +10,32 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 };
 
-export function Question(props: QuestionProps) {
+export function Question({
+  content,
+  author,
+  children,
+  isAnswered = false,
+  isHighlighted = false,
+}: QuestionProps) {
   return (
-    <div className="question">
-      <p>{props.content}</p>
+    <div
+      className={cx(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered }
+      )}
+    >
+      <p>{content}</p>
       <footer>
         <div className="user-info">
-          <img src={props.author.avatar} alt={props.author.name} />
-          <span>{props.author.name}</span>
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
         </div>
-        <div>
-          {props.children}
-        </div>
+        <div>{children}</div>
       </footer>
     </div>
   );
